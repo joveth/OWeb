63ebac006d3e3d650c4a5cf4dc6741291f24b1ad
jov <ominds@163.com>
2014-05-15 12:13:10 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 module.exports = function(app) {
 	app.get('/', exports.Index);
 	app.get('/infor', exports.Infor);
+	app.get('/about', exports.About);
 };
 exports.Index = function(req, res){
 	res.render('index', { title: 'Home - OWeb' });
 };
 exports.Infor = function(req, res){
 	res.render('oinfor', { title: 'Infor - OWeb' });
+};
+exports.About = function(req, res){
+	res.render('about', { title: 'About - OWeb' });
 };
